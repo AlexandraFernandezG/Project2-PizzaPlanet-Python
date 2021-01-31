4a71e6582bee531ad5ae287fa8cd7149221577a9
vanessanadal <vvnadal.17@est.ucab.edu.ve>
2021-01-31 00:52:21 -0400

    Agregando modelo y configuraciones del mismo

--- pizzeria/pizzaplanet/admin.py
+++ pizzeria/pizzaplanet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Cliente, Delivery, Bebida, Pedido, Pago, Calificacion, Tamaño, Ingrediente, Pizza, Ingrediente_pizza
+
+admin.site.register(Cliente)
+admin.site.register(Delivery)
+admin.site.register(Bebida)
+admin.site.register(Pedido)
+admin.site.register(Pago)
+admin.site.register(Calificacion)
+admin.site.register(Tamaño)
+admin.site.register(Ingrediente)
+admin.site.register(Pizza)
+admin.site.register(Ingrediente_pizza)
--- pizzeria/pizzaplanet/models.py
+++ pizzeria/pizzaplanet/models.py
@@ -3,11 +3,66 @@ from django.db import models
 # Create your models here.
 #Aqui van las tablas de BD
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('fecha publicación')
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=200)
+    cedula =  models.CharField(max_length=200)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.nombre
+
+class Delivery(models.Model):
+    direccion = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Bebida(models.Model):
+    tipo = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.tipo
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    bebida = models.ForeignKey(Bebida, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    fecha = models.DateTimeField('Fecha pedido')
+
+class Pago(models.Model):
+    tipo_pago = models.CharField(max_length=200)
+    monto = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Calificacion(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    comentario = models.CharField(max_length=500)
+    puntuacion = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.comentario
+
+class Tamaño(models.Model):
+    tipo = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.tipo
+
+
+class Ingrediente(models.Model):
+    nombre = models.CharField(max_length=200)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Pizza(models.Model):
+    tamaño = models.ForeignKey(Tamaño, on_delete=models.CASCADE)
+    simple = models.BooleanField(default=True)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Ingrediente_pizza(models.Model):
+    ingrediente = models.ForeignKey(Ingrediente, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+
+
