e710a6260a31726fc1427acda3741a2875da476a
vanessanadal <vvnadal.17@est.ucab.edu.ve>
2021-01-31 23:03:57 -0400

    Uniendo cambios

--- pizzeria/pizzaplanet/views.py
+++ pizzeria/pizzaplanet/views.py
@@ -1,9 +1,32 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Tamano, Ingrediente, Bebida
 
 def inicio(request):
     return render(request, 'pizzaplanet/inicio.html')
 
 def pedidos(request):
-    return render(request, 'pizzaplanet/pedidos.html')
+    tamanos = Tamano.objects.all()
+    ingredientes = Ingrediente.objects.all()
+    bebidas = Bebida.objects.all()
+
+    ingredientes_temp = []
+    bebidas_temp = []
+    tamanos_temp = []
+
+    for ingrediente in ingredientes:
+        ingredientes_temp.append({"id": ingrediente.id, "nombre": ingrediente.nombre, "precio": ingrediente.precio})
+  
+    for tamano in tamanos:
+        tamanos_temp.append({"id": tamano.id, "tipo": tamano.tipo, "precio": tamano.precio})
+  
+    for bebida in bebidas:
+        bebidas_temp.append({"id": bebida.id, "tipo": bebida.tipo, "precio": bebida.precio})
+
+    context = {
+        "tamanos": tamanos,
+        "ingredientes": ingredientes,
+        "bebida" : bebida,
+    }
+    return render(request, 'pizzaplanet/pedidos.html', context)
