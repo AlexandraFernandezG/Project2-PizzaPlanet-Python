0cb24aaf3749d29ba0c5813cae66e30e8444114e
odavid10 <oder1011119504@gmail.com>
2021-01-31 20:14:05 -0400

    avances en diseño del html pedido

--- pizzeria/pizzaplanet/urls.py
+++ pizzeria/pizzaplanet/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
-    path('catalogo', views.catalogo, name="catalogo"),
-    path('pedidos', views.pedidos, name="pedidos"),   #En views busca funcion index. Ya tengo ruta para llegarle a index
+    path('pedidos', views.pedidos, name="pedidos"),
 ]
--- pizzeria/pizzaplanet/views.py
+++ pizzeria/pizzaplanet/views.py
@@ -5,8 +5,5 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'pizzaplanet/inicio.html')
 
-def catalogo(request):
-    return render(request, 'pizzaplanet/catalogo.html')
-
 def pedidos(request):
     return render(request, 'pizzaplanet/pedidos.html')
